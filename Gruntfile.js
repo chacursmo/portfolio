6e710e114c104170dd35dd79f2e823b5babdd3cf
Sam Crouch <chacursmo@gmail.com>
2016-05-21 13:18:08 -0500

    Another Initial Commit: Yeah Getting the Hang of this get things

---
+++ Gruntfile.js
@@ -0,0 +1,14 @@
+module.export = function(grunt) {
+    grunt.initConfig({
+	jshint: {
+	    files: ['Gruntfile.js','src/**/*.js','test/**/*.js'],
+	    options: {
+		globals: {
+		    jQuery: true
+		}
+	    }
+	},
+	watch: {
+	    files: ['
+   
+}
---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp')
+
+gulp.task('default', function() {
+    console.log('Hello World');
+});
+
+
+gulp.task('robot', function(){
+    console.log('R A Pirate Robot');
+});
